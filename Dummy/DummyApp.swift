47e16009ff5c86a771e3b795501e7ae3b3102d82
Khalil <ADMIN@MacBook-Pro-de-SLIM.local>
2023-06-10 13:14:58 +0100

    first commit

---
+++ Dummy/DummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyApp.swift
+//  Dummy
+//
+//  Created by Khalil on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
